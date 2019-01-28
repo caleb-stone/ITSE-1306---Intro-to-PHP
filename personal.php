c9228eb2bc65812126c6875e50488f26939b22fe
caleb-stone <46629173+caleb-stone@users.noreply.github.com>
2019-01-27 21:33:29 -0600

    Add files via upload

---
+++ personal.php
@@ -0,0 +1,40 @@
+<?php
+// --------------
+// Programmer:  Caleb Stone
+// Course:      ITSE-1306 (Intro to PHP)
+// Instructor:  Cesar "Coach" Marrero
+// Assignment:  Week 2 - Lab
+// Description: Week 2 - Lab - Hello World & Photo
+// --------------
+echo '<!--';
+echo '--------------';
+echo 'Programmer:  Caleb Stone';
+echo 'Course:      ITSE-1306 (Intro to PHP)';
+echo 'Instructor:  Cesar "Coach" Marrero';
+echo 'Assignment:  Week 2 - Lab';
+echo 'Description: Week 2 - Lab - Hello World & Photo';
+echo '--------------';
+echo '-->';
+echo '';
+echo '<!DOCTYPE html>';
+echo '<html>';
+echo '  <head>';
+echo '    <link rel="stylesheet" href="personal.css">';
+echo '    <title>Student Personal Page</title>';
+echo '  </head>';
+echo '  <body>';
+echo '    ';
+echo '	<h1>Caleb Stone</h1>';
+echo '	<p>Hello World, my name is Caleb Stone</p>';
+echo '  ';
+echo '    <a href="IMG_4549_cropped_small.JPG">';
+echo '     <img src="IMG_4549_cropped_small.JPG" alt="Photo">';
+echo '    </a><br>';
+echo '    ';
+echo '    <br>';
+echo '    ';
+echo '    <a href=\'http://google.com\'>Link to Google</a><br>';
+echo '    <a href=\'nowhere\'>Link to nowhere</a><br>';
+echo '  </body>';
+echo '</html>';
+?>
